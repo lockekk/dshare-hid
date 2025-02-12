deskflow/deskflow

refactor: secureutils add pemFileCertFingerprint

src/lib/net/SecureUtils.cpp
@@ -7,6 +7,8 @@
 
 #include "SecureUtils.h"
 #include "base/String.h"
+#include "base/finally.h"
+#include "io/filesystem.h"
 
 #include <openssl/pem.h>
 #include <openssl/x509.h>
@@ -63,4 +65,20 @@ std::vector<uint8_t> SSLCertFingerprint(X509 *cert, FingerprintType type)
   return digest_vec;
 }
 
+std::vector<std::uint8_t> pemFileCertFingerprint(const std::string &path, FingerprintType type)
+{
+  auto fp = fopenUtf8Path(path, "r");
+  if (!fp) {
+    throw std::runtime_error("Could not open certificate path");
+  }
+  auto file_close = finally([fp]() { std::fclose(fp); });
+
+  X509 *cert = PEM_read_X509(fp, nullptr, nullptr, nullptr);
+  if (!cert) {
+    throw std::runtime_error("Certificate could not be parsed");
+  }
+  auto cert_free = finally([cert]() { X509_free(cert); });
+
+  return SSLCertFingerprint(cert, type);
+}
 } // namespace deskflow

lockekk/dshare-hid

src/lib/net/SecureUtils.cpp
@@ -7,6 +7,8 @@
 
 #include "SecureUtils.h"
 #include "base/String.h"
+#include "base/finally.h"
+#include "io/filesystem.h"
 
 #include <openssl/pem.h>
 #include <openssl/x509.h>
@@ -66,4 +68,20 @@ std::vector<uint8_t> SSLCertFingerprint(X509 *cert, FingerprintType type)
   return digestVec;
 }
 
+std::vector<std::uint8_t> pemFileCertFingerprint(const std::string &path, FingerprintType type)
+{
+  auto fp = fopenUtf8Path(path, "r");
+  if (!fp) {
+    throw std::runtime_error("could not open certificate path");
+  }
+  auto fileClose = finally([fp]() { std::fclose(fp); });
+
+  X509 *cert = PEM_read_X509(fp, nullptr, nullptr, nullptr);
+  if (!cert) {
+    throw std::runtime_error("certificate could not be parsed");
+  }
+  auto certFree = finally([cert]() { X509_free(cert); });
+
+  return SSLCertFingerprint(cert, type);
+}
 } // namespace deskflow