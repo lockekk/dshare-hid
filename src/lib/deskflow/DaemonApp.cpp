deskflow/deskflow

feat: Restart process from daemon through new Qt IPC impl

src/lib/deskflow/DaemonApp.cpp
@@ -106,12 +106,39 @@ DaemonApp::DaemonApp(int argc, char **argv) : QCoreApplication(argc, argv), m_ip
   if (init(argc, argv) != kExitSuccess) {
     exit(kExitFailed);
   }
+
+  connect(m_ipcServer2, &ipc::DaemonIpcServer::elevateModeChanged, this, &DaemonApp::handleElevateModeChanged);
+  connect(m_ipcServer2, &ipc::DaemonIpcServer::commandChanged, this, &DaemonApp::handleCommandChanged);
+  connect(m_ipcServer2, &ipc::DaemonIpcServer::restartRequested, this, &DaemonApp::handleRestartRequested);
+
   s_instance = this;
 }
 
 DaemonApp::~DaemonApp()
 {
   s_instance = nullptr;
+}
+
+void DaemonApp::handleElevateModeChanged(int mode)
+{
+  LOG((CLOG_DEBUG "elevate mode changed: %d", mode));
+  m_elevateMode = mode;
+}
+
+void DaemonApp::handleCommandChanged(const QString &command)
+{
+  LOG((CLOG_INFO "service command updated"));
+  m_command = command.toStdString();
+}
+
+void DaemonApp::handleRestartRequested()
+{
+  LOG((CLOG_INFO "service restart requested"));
+#if SYSAPI_WIN32
+  m_watchdog->setCommand(m_command, m_elevateMode);
+#else
+  LOG_ERR("restart not implemented on this platform");
+#endif
 }
 
 void DaemonApp::startAsync()

src/lib/deskflow/DaemonApp.h
@@ -43,6 +43,11 @@ private:
   std::string logFilename();
   void handleIpcMessage(const Event &, void *);
 
+private slots:
+  void handleElevateModeChanged(int mode);
+  void handleCommandChanged(const QString &command);
+  void handleRestartRequested();
+
 public:
   static DaemonApp *s_instance;
 
@@ -55,5 +60,7 @@ private:
   std::unique_ptr<IpcLogOutputter> m_ipcLogOutputter;
   std::unique_ptr<IEventQueue> m_events;
   std::unique_ptr<FileLogOutputter> m_fileLogOutputter;
-  std::unique_ptr<deskflow::core::ipc::DaemonIpcServer> m_ipcServer2;
+  deskflow::core::ipc::DaemonIpcServer *m_ipcServer2 = nullptr;
+  std::string m_command = "";
+  int m_elevateMode = 0;
 };

src/lib/deskflow/ipc/DaemonIpcServer.cpp
@@ -39,28 +39,93 @@ void DaemonIpcServer::handleNewConnection()
   }
 
   LOG_INFO("ipc server got new connection");
+  m_clients.insert(clientSocket);
 
-  connect(clientSocket, &QLocalSocket::readyRead, this, [clientSocket]() {
-    LOG_DEBUG("ipc server ready to read data");
+  connect(clientSocket, &QLocalSocket::readyRead, this, &DaemonIpcServer::handleReadyRead);
+  connect(clientSocket, &QLocalSocket::disconnected, this, &DaemonIpcServer::handleDisconnected);
+  connect(clientSocket, &QLocalSocket::errorOccurred, this, &DaemonIpcServer::handleErrorOccurred);
+}
 
-    QByteArray data = clientSocket->readAll();
-    if (data.isEmpty()) {
-      LOG_WARN("ipc server got empty message");
+void DaemonIpcServer::handleReadyRead()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_DEBUG("ipc server ready to read data");
+
+  QByteArray data = clientSocket->readAll();
+  if (data.isEmpty()) {
+    LOG_WARN("ipc server got empty message");
+    return;
+  }
+
+  // we don't handle incomplete messages yet; each socket read must have delimiters.
+  if (!data.contains('\n')) {
+    LOG_WARN("ipc server got incomplete message: %s", data.constData());
+    return;
+  }
+
+  // each message is delimited by a newline to keep the protocol super simple.
+  while (data.contains('\n')) {
+    int index = data.indexOf('\n');
+    QByteArray messageData = data.left(index);
+    data.remove(0, index + 1);
+    QString message = QString::fromUtf8(messageData);
+    processMessage(clientSocket, message);
+  }
+}
+
+void DaemonIpcServer::handleDisconnected()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_INFO("ipc server client disconnected");
+  m_clients.remove(clientSocket);
+  clientSocket->deleteLater();
+}
+
+void DaemonIpcServer::handleErrorOccurred()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_ERR("ipc server client error: %s", clientSocket->errorString().toUtf8().constData());
+  m_clients.remove(clientSocket);
+  clientSocket->deleteLater();
+}
+
+void DaemonIpcServer::processMessage(QLocalSocket *clientSocket, const QString &message)
+{
+  const auto kAckMessage = "ok\n";
+  const auto kErrorMessage = "error\n";
+
+  LOG_DEBUG("ipc server got message: %s", message.toUtf8().constData());
+  if (message == "hello") {
+    clientSocket->write("hello\n");
+  } else if (message.startsWith("elevate=")) {
+    const auto elevateMode = message.split('=')[1].toInt();
+    if (elevateMode < 0 || elevateMode > 2) {
+      LOG_ERR("ipc server got invalid elevate mode: %d", elevateMode);
+      clientSocket->write(kErrorMessage);
       return;
+    } else {
+      LOG_INFO("ipc server got new elevate mode: %d", elevateMode);
+      Q_SIGNAL elevateModeChanged(elevateMode);
+      clientSocket->write(kAckMessage);
     }
-
-    QString dataStr = QString::fromUtf8(data);
-    if (dataStr.isEmpty()) {
-      LOG_ERR("ipc server failed to convert message to string");
-      return;
+  } else if (message.startsWith("command=")) {
+    const auto command = message.split('=')[1];
+    if (command.isEmpty()) {
+      LOG_ERR("ipc server got empty command");
+      clientSocket->write(kErrorMessage);
+    } else {
+      LOG_INFO("ipc server got new command: %s", command.toUtf8().constData());
+      Q_SIGNAL commandChanged(command);
+      clientSocket->write(kAckMessage);
     }
-
-    if (dataStr == "hello") {
-      LOG_DEBUG("ipc server got message: %s", data.constData());
-      clientSocket->write("hello");
-      clientSocket->flush();
-    }
-  });
+  } else if (message == "restart") {
+    LOG_INFO("ipc server got restart message");
+    Q_SIGNAL restartRequested();
+    clientSocket->write(kAckMessage);
+  } else {
+    LOG_WARN("ipc server got unknown message: %s", message.toUtf8().constData());
+  }
+  clientSocket->flush();
 }
 
 } // namespace deskflow::core::ipc

lockekk/dshare-hid

src/lib/deskflow/DaemonApp.cpp
@@ -101,12 +101,38 @@ DaemonApp::DaemonApp(IEventQueue *events, int argc, char **argv)
       m_events(events),
       m_ipcServer{new ipc::DaemonIpcServer(this)}
 {
+  connect(m_ipcServer, &ipc::DaemonIpcServer::elevateModeChanged, this, &DaemonApp::handleElevateModeChanged);
+  connect(m_ipcServer, &ipc::DaemonIpcServer::commandChanged, this, &DaemonApp::handleCommandChanged);
+  connect(m_ipcServer, &ipc::DaemonIpcServer::restartRequested, this, &DaemonApp::handleRestartRequested);
+
   s_instance = this;
 }
 
 DaemonApp::~DaemonApp()
 {
   s_instance = nullptr;
+}
+
+void DaemonApp::handleElevateModeChanged(int mode)
+{
+  LOG_DEBUG("elevate mode changed: %d", mode);
+  m_elevateMode = mode;
+}
+
+void DaemonApp::handleCommandChanged(const QString &command)
+{
+  LOG_DEBUG("service command updated");
+  m_command = command.toStdString();
+}
+
+void DaemonApp::handleRestartRequested()
+{
+  LOG_DEBUG("service restart requested");
+#if SYSAPI_WIN32
+  m_watchdog->setCommand(m_command, m_elevateMode);
+#else
+  LOG_ERR("restart not implemented on this platform");
+#endif
 }
 
 void DaemonApp::startAsync()
@@ -205,7 +231,7 @@ void DaemonApp::mainLoop(bool logToFile, bool foreground)
     std::string command = ARCH->setting("Command");
     bool elevate = ARCH->setting("Elevate") == "1";
     if (command != "") {
-      LOG((CLOG_INFO "using last known command: %s", command.c_str()));
+      LOG_DEBUG("using last known command: %s", command.c_str());
       m_watchdog->setCommand(command, elevate);
     }
 
@@ -216,8 +242,6 @@ void DaemonApp::mainLoop(bool logToFile, bool foreground)
 #if SYSAPI_WIN32
     m_watchdog->stop();
 #endif
-
-    m_events->removeHandler(m_events->forIpcServer().messageReceived(), m_ipcServer.get());
 
     CLOG->remove(m_ipcLogOutputter.get());
 

src/lib/deskflow/DaemonApp.h
@@ -13,6 +13,8 @@
 
 #include <QCoreApplication>
 
+#include "common/common.h"
+
 class Event;
 class IEventQueue;
 class IpcLogOutputter;
@@ -44,6 +46,11 @@ private:
   std::string logFilename();
   void handleIpcMessage(const Event &, void *);
 
+private slots:
+  void handleElevateModeChanged(int mode);
+  void handleCommandChanged(const QString &command);
+  void handleRestartRequested();
+
 public:
   static DaemonApp *s_instance;
 
@@ -55,5 +62,7 @@ private:
   std::unique_ptr<IpcLogOutputter> m_ipcLogOutputter;
   IEventQueue *m_events = nullptr;
   FileLogOutputter *m_fileLogOutputter = nullptr;
-  std::unique_ptr<deskflow::core::ipc::DaemonIpcServer> m_ipcServer;
+  deskflow::core::ipc::DaemonIpcServer *m_ipcServer = nullptr;
+  std::string m_command = "";
+  int m_elevateMode = 0;
 };

src/lib/deskflow/ipc/DaemonIpcServer.cpp
@@ -42,28 +42,93 @@ void DaemonIpcServer::handleNewConnection()
   }
 
   LOG_DEBUG("ipc server got new connection");
+  m_clients.insert(clientSocket);
 
-  connect(clientSocket, &QLocalSocket::readyRead, this, [clientSocket]() {
-    LOG_DEBUG("ipc server ready to read data");
+  connect(clientSocket, &QLocalSocket::readyRead, this, &DaemonIpcServer::handleReadyRead);
+  connect(clientSocket, &QLocalSocket::disconnected, this, &DaemonIpcServer::handleDisconnected);
+  connect(clientSocket, &QLocalSocket::errorOccurred, this, &DaemonIpcServer::handleErrorOccurred);
+}
 
-    QByteArray data = clientSocket->readAll();
-    if (data.isEmpty()) {
-      LOG_WARN("ipc server got empty message");
+void DaemonIpcServer::handleReadyRead()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_DEBUG("ipc server ready to read data");
+
+  QByteArray data = clientSocket->readAll();
+  if (data.isEmpty()) {
+    LOG_WARN("ipc server got empty message");
+    return;
+  }
+
+  // we don't handle incomplete messages yet; each socket read must have delimiters.
+  if (!data.contains('\n')) {
+    LOG_WARN("ipc server got incomplete message: %s", data.constData());
+    return;
+  }
+
+  // each message is delimited by a newline to keep the protocol super simple.
+  while (data.contains('\n')) {
+    int index = data.indexOf('\n');
+    QByteArray messageData = data.left(index);
+    data.remove(0, index + 1);
+    QString message = QString::fromUtf8(messageData);
+    processMessage(clientSocket, message);
+  }
+}
+
+void DaemonIpcServer::handleDisconnected()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_DEBUG("ipc server client disconnected");
+  m_clients.remove(clientSocket);
+  clientSocket->deleteLater();
+}
+
+void DaemonIpcServer::handleErrorOccurred()
+{
+  const auto clientSocket = qobject_cast<QLocalSocket *>(sender());
+  LOG_ERR("ipc server client error: %s", clientSocket->errorString().toUtf8().constData());
+  m_clients.remove(clientSocket);
+  clientSocket->deleteLater();
+}
+
+void DaemonIpcServer::processMessage(QLocalSocket *clientSocket, const QString &message)
+{
+  const auto kAckMessage = "ok\n";
+  const auto kErrorMessage = "error\n";
+
+  LOG_DEBUG("ipc server got message: %s", message.toUtf8().constData());
+  if (message == "hello") {
+    clientSocket->write("hello\n");
+  } else if (message.startsWith("elevate=")) {
+    const auto elevateMode = message.split('=')[1].toInt();
+    if (elevateMode < 0 || elevateMode > 2) {
+      LOG_ERR("ipc server got invalid elevate mode: %d", elevateMode);
+      clientSocket->write(kErrorMessage);
       return;
+    } else {
+      LOG_DEBUG("ipc server got new elevate mode: %d", elevateMode);
+      Q_SIGNAL elevateModeChanged(elevateMode);
+      clientSocket->write(kAckMessage);
     }
-
-    QString dataStr = QString::fromUtf8(data);
-    if (dataStr.isEmpty()) {
-      LOG_ERR("ipc server failed to convert message to string");
-      return;
+  } else if (message.startsWith("command=")) {
+    const auto command = message.split('=')[1];
+    if (command.isEmpty()) {
+      LOG_ERR("ipc server got empty command");
+      clientSocket->write(kErrorMessage);
+    } else {
+      LOG_DEBUG("ipc server got new command: %s", command.toUtf8().constData());
+      Q_SIGNAL commandChanged(command);
+      clientSocket->write(kAckMessage);
     }
-
-    if (dataStr == "hello") {
-      LOG_DEBUG("ipc server got message: %s", data.constData());
-      clientSocket->write("hello");
-      clientSocket->flush();
-    }
-  });
+  } else if (message == "restart") {
+    LOG_DEBUG("ipc server got restart message");
+    Q_SIGNAL restartRequested();
+    clientSocket->write(kAckMessage);
+  } else {
+    LOG_WARN("ipc server got unknown message: %s", message.toUtf8().constData());
+  }
+  clientSocket->flush();
 }
 
 } // namespace deskflow::core::ipc