deskflow/deskflow

refactor: XArch Classes => Arch Exception  Classes

src/lib/arch/ArchException.h
@@ -1,0 +1,205 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
+ * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
+ * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#pragma once
+
+#include "common/Common.h"
+
+#include <stdexcept>
+#include <string>
+
+//! Generic thread exception
+/*!
+Exceptions derived from this class are used by the multithreading
+library to perform stack unwinding when a thread terminates.  These
+exceptions must always be rethrown by clients when caught.
+*/
+class ThreadException : public std::exception
+{
+};
+
+//! Thread exception to cancel
+/*!
+Thrown to cancel a thread.  Clients must not throw this type, but
+must rethrow it if caught (by ThreadCancelException, ThreadException, or ...).
+*/
+class ThreadCancelException : public ThreadException
+{
+};
+
+/*!
+\def RETHROW_THREADEXCEPTION
+Convenience macro to rethrow an ThreadException exception but ignore other
+exceptions.  Put this in your catch (...) handler after necessary
+cleanup but before leaving or returning from the handler.
+*/
+#define RETHROW_THREADEXCEPTION                                                                                        \
+  try {                                                                                                                \
+    throw;                                                                                                             \
+  } catch (ThreadException &) {                                                                                        \
+    throw;                                                                                                             \
+  } catch (...) {                                                                                                      \
+  }
+
+//! Generic network exception
+/*!
+Exceptions derived from this class are used by the networking
+library to indicate various errors.
+*/
+class ArchNetworkException : public std::runtime_error
+{
+  using std::runtime_error::runtime_error;
+};
+
+//! Operation was interrupted
+class ArchNetworkInterruptedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network insufficient permission
+class ArchNetworkAccessException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network insufficient resources
+class ArchNetworkResourceException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! No support for requested network resource/service
+class ArchNetworkSupportException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network I/O error
+class ArchNetworkIOException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network address is unavailable or not local
+class ArchNetworkNoAddressException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network address in use
+class ArchNetworkAddressInUseException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! No route to address
+class ArchNetworkNoRouteException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Socket not connected
+class ArchNetworkNotConnectedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote read end of socket has closed
+class ArchNetworkShutdownException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket has disconnected
+class ArchNetworkDisconnectedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket refused connection
+class ArchNetworkConnectionRefusedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket is not responding
+class ArchNetworkTimedOutException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Generic network name lookup erros
+class ArchNetworkNameException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! The named host is unknown
+class ArchNetworkNameUnknownException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! The named host is known but has no address
+class ArchNetworkNameNoAddressException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Non-recoverable name server error
+class ArchNetworkNameFailureException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Temporary name server error
+class ArchNetworkNameUnavailableException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! The named host is known but no supported address
+class ArchNetworkNameUnsupportedException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Generic daemon exception
+/*!
+Exceptions derived from this class are used by the daemon
+library to indicate various errors.
+*/
+class ArchDaemonException : public std::runtime_error
+{
+  using std::runtime_error::runtime_error;
+};
+
+//! Could not daemonize
+class ArchDaemonFailedException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Could not install daemon
+class ArchDaemonInstallFailedException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Could not uninstall daemon
+class ArchDaemonUninstallFailedException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Attempted to uninstall a daemon that was not installed
+class ArchDaemonUninstallNotInstalledException : public ArchDaemonFailedException
+{
+  using ArchDaemonFailedException::ArchDaemonFailedException;
+};

src/lib/arch/win32/ArchDaemonWindows.cpp
@@ -8,7 +8,7 @@
 #include "arch/win32/ArchDaemonWindows.h"
 
 #include "arch/Arch.h"
-#include "arch/XArch.h"
+#include "arch/ArchException.h"
 #include "arch/win32/ArchMiscWindows.h"
 #include "arch/win32/XArchWindows.h"
 #include "base/Log.h"
@@ -51,7 +51,7 @@ UINT ArchDaemonWindows::getDaemonQuitMessage()
 void ArchDaemonWindows::daemonFailed(int result)
 {
   assert(s_daemon != nullptr);
-  throw XArchDaemonRunFailed(result);
+  throw ArchDaemonExceptionRunFailed(result);
 }
 
 void ArchDaemonWindows::installDaemon(
@@ -64,7 +64,7 @@ void ArchDaemonWindows::installDaemon(
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_WRITE);
   if (mgr == nullptr) {
     // can't open service manager
-    throw XArchDaemonInstallFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonInstallFailedException(windowsErrorToString(GetLastError()));
   }
 
   // create the service
@@ -78,7 +78,7 @@ void ArchDaemonWindows::installDaemon(
     DWORD err = GetLastError();
     if (err != ERROR_SERVICE_EXISTS) {
       CloseServiceHandle(mgr);
-      throw XArchDaemonInstallFailed(windowsErrorToString(err));
+      throw ArchDaemonInstallFailedException(windowsErrorToString(err));
     }
   } else {
     // done with service (but only try to close if not null)
@@ -99,7 +99,7 @@ void ArchDaemonWindows::installDaemon(
     } catch (...) {
       // ignore
     }
-    throw XArchDaemonInstallFailed(windowsErrorToString(err));
+    throw ArchDaemonInstallFailedException(windowsErrorToString(err));
   }
 
   // set the description
@@ -116,7 +116,7 @@ void ArchDaemonWindows::installDaemon(
     } catch (...) {
       // ignore
     }
-    throw XArchDaemonInstallFailed(windowsErrorToString(err));
+    throw ArchDaemonInstallFailedException(windowsErrorToString(err));
   }
   ArchMiscWindows::setValue(key, _T("CommandLine"), commandLine);
 
@@ -140,7 +140,7 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_WRITE);
   if (mgr == nullptr) {
     // can't open service manager
-    throw XArchDaemonUninstallFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonUninstallFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service.  oddly, you must open a service to delete it.
@@ -149,9 +149,9 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
     DWORD err = GetLastError();
     CloseServiceHandle(mgr);
     if (err != ERROR_SERVICE_DOES_NOT_EXIST) {
-      throw XArchDaemonUninstallFailed(windowsErrorToString(err));
+      throw ArchDaemonUninstallFailedException(windowsErrorToString(err));
     }
-    throw XArchDaemonUninstallNotInstalled(windowsErrorToString(err));
+    throw ArchDaemonUninstallNotInstalledException(windowsErrorToString(err));
   }
 
   // stop the service.  we don't care if we fail.
@@ -184,9 +184,9 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
       return;
     }
     if (err != ERROR_SERVICE_MARKED_FOR_DELETE) {
-      throw XArchDaemonUninstallFailed(windowsErrorToString(err));
+      throw ArchDaemonUninstallFailedException(windowsErrorToString(err));
     }
-    throw XArchDaemonUninstallNotInstalled(windowsErrorToString(err));
+    throw ArchDaemonUninstallNotInstalledException(windowsErrorToString(err));
   }
 }
 
@@ -211,7 +211,7 @@ int ArchDaemonWindows::daemonize(const char *name, DaemonFunc const &func)
   if (StartServiceCtrlDispatcher(entry) == 0) {
     // StartServiceCtrlDispatcher failed
     s_daemon = nullptr;
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   s_daemon = nullptr;
@@ -472,7 +472,7 @@ void ArchDaemonWindows::serviceMain(DWORD argc, LPTSTR *argvIn)
   try {
     // invoke daemon function
     m_daemonResult = m_daemonFunc(static_cast<int>(argc), argv);
-  } catch (XArchDaemonRunFailed &e) {
+  } catch (ArchDaemonExceptionRunFailed &e) {
     setStatusError(e.m_result);
     m_daemonResult = -1;
   } catch (...) {
@@ -560,7 +560,7 @@ void ArchDaemonWindows::start(const char *name)
   // open service manager
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_READ);
   if (mgr == nullptr) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service
@@ -568,12 +568,12 @@ void ArchDaemonWindows::start(const char *name)
 
   if (service == nullptr) {
     CloseServiceHandle(mgr);
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // start the service
   if (!StartService(service, 0, nullptr)) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 }
 
@@ -582,7 +582,7 @@ void ArchDaemonWindows::stop(const char *name)
   // open service manager
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_READ);
   if (mgr == nullptr) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service
@@ -590,7 +590,7 @@ void ArchDaemonWindows::stop(const char *name)
 
   if (service == nullptr) {
     CloseServiceHandle(mgr);
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // ask the service to stop, asynchronously
@@ -598,7 +598,7 @@ void ArchDaemonWindows::stop(const char *name)
   if (!ControlService(service, SERVICE_CONTROL_STOP, &ss)) {
     DWORD dwErrCode = GetLastError();
     if (dwErrCode != ERROR_SERVICE_NOT_ACTIVE) {
-      throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+      throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
     }
   }
 }

lockekk/dshare-hid

src/lib/arch/ArchException.h
@@ -1,0 +1,205 @@
+/*
+ * Deskflow -- mouse and keyboard sharing utility
+ * SPDX-FileCopyrightText: (C) 2025 Deskflow Developers
+ * SPDX-FileCopyrightText: (C) 2012 - 2016 Symless Ltd.
+ * SPDX-FileCopyrightText: (C) 2002 Chris Schoeneman
+ * SPDX-License-Identifier: GPL-2.0-only WITH LicenseRef-OpenSSL-Exception
+ */
+
+#pragma once
+
+#include "common/Common.h"
+
+#include <stdexcept>
+#include <string>
+
+//! Generic thread exception
+/*!
+Exceptions derived from this class are used by the multithreading
+library to perform stack unwinding when a thread terminates.  These
+exceptions must always be rethrown by clients when caught.
+*/
+class ThreadException : public std::exception
+{
+};
+
+//! Thread exception to cancel
+/*!
+Thrown to cancel a thread.  Clients must not throw this type, but
+must rethrow it if caught (by ThreadCancelException, ThreadException, or ...).
+*/
+class ThreadCancelException : public ThreadException
+{
+};
+
+/*!
+\def RETHROW_THREADEXCEPTION
+Convenience macro to rethrow an ThreadException exception but ignore other
+exceptions.  Put this in your catch (...) handler after necessary
+cleanup but before leaving or returning from the handler.
+*/
+#define RETHROW_THREADEXCEPTION                                                                                        \
+  try {                                                                                                                \
+    throw;                                                                                                             \
+  } catch (ThreadException &) {                                                                                        \
+    throw;                                                                                                             \
+  } catch (...) {                                                                                                      \
+  }
+
+//! Generic network exception
+/*!
+Exceptions derived from this class are used by the networking
+library to indicate various errors.
+*/
+class ArchNetworkException : public std::runtime_error
+{
+  using std::runtime_error::runtime_error;
+};
+
+//! Operation was interrupted
+class ArchNetworkInterruptedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network insufficient permission
+class ArchNetworkAccessException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network insufficient resources
+class ArchNetworkResourceException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! No support for requested network resource/service
+class ArchNetworkSupportException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network I/O error
+class ArchNetworkIOException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network address is unavailable or not local
+class ArchNetworkNoAddressException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Network address in use
+class ArchNetworkAddressInUseException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! No route to address
+class ArchNetworkNoRouteException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Socket not connected
+class ArchNetworkNotConnectedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote read end of socket has closed
+class ArchNetworkShutdownException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket has disconnected
+class ArchNetworkDisconnectedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket refused connection
+class ArchNetworkConnectionRefusedException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Remote end of socket is not responding
+class ArchNetworkTimedOutException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! Generic network name lookup erros
+class ArchNetworkNameException : public ArchNetworkException
+{
+  using ArchNetworkException::ArchNetworkException;
+};
+
+//! The named host is unknown
+class ArchNetworkNameUnknownException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! The named host is known but has no address
+class ArchNetworkNameNoAddressException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Non-recoverable name server error
+class ArchNetworkNameFailureException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Temporary name server error
+class ArchNetworkNameUnavailableException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! The named host is known but no supported address
+class ArchNetworkNameUnsupportedException : public ArchNetworkNameException
+{
+  using ArchNetworkNameException::ArchNetworkNameException;
+};
+
+//! Generic daemon exception
+/*!
+Exceptions derived from this class are used by the daemon
+library to indicate various errors.
+*/
+class ArchDaemonException : public std::runtime_error
+{
+  using std::runtime_error::runtime_error;
+};
+
+//! Could not daemonize
+class ArchDaemonFailedException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Could not install daemon
+class ArchDaemonInstallException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Could not uninstall daemon
+class ArchDaemonUninstallFailedException : public ArchDaemonException
+{
+  using ArchDaemonException::ArchDaemonException;
+};
+
+//! Attempted to uninstall a daemon that was not installed
+class ArchDaemonUninstallNotInstalledException : public ArchDaemonFailedException
+{
+  using ArchDaemonFailedException::ArchDaemonFailedException;
+};

src/lib/arch/win32/ArchDaemonWindows.cpp
@@ -8,7 +8,7 @@
 #include "arch/win32/ArchDaemonWindows.h"
 
 #include "arch/Arch.h"
-#include "arch/XArch.h"
+#include "arch/ArchException.h"
 #include "arch/win32/ArchMiscWindows.h"
 #include "arch/win32/XArchWindows.h"
 #include "base/Log.h"
@@ -51,7 +51,7 @@ UINT ArchDaemonWindows::getDaemonQuitMessage()
 void ArchDaemonWindows::daemonFailed(int result)
 {
   assert(s_daemon != nullptr);
-  throw XArchDaemonRunFailed(result);
+  throw ArchDaemonRunException(result);
 }
 
 void ArchDaemonWindows::installDaemon(
@@ -64,7 +64,7 @@ void ArchDaemonWindows::installDaemon(
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_WRITE);
   if (mgr == nullptr) {
     // can't open service manager
-    throw XArchDaemonInstallFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonInstallException(windowsErrorToString(GetLastError()));
   }
 
   // create the service
@@ -78,7 +78,7 @@ void ArchDaemonWindows::installDaemon(
     DWORD err = GetLastError();
     if (err != ERROR_SERVICE_EXISTS) {
       CloseServiceHandle(mgr);
-      throw XArchDaemonInstallFailed(windowsErrorToString(err));
+      throw ArchDaemonInstallException(windowsErrorToString(err));
     }
   } else {
     // done with service (but only try to close if not null)
@@ -99,7 +99,7 @@ void ArchDaemonWindows::installDaemon(
     } catch (...) {
       // ignore
     }
-    throw XArchDaemonInstallFailed(windowsErrorToString(err));
+    throw ArchDaemonInstallException(windowsErrorToString(err));
   }
 
   // set the description
@@ -116,7 +116,7 @@ void ArchDaemonWindows::installDaemon(
     } catch (...) {
       // ignore
     }
-    throw XArchDaemonInstallFailed(windowsErrorToString(err));
+    throw ArchDaemonInstallException(windowsErrorToString(err));
   }
   ArchMiscWindows::setValue(key, _T("CommandLine"), commandLine);
 
@@ -140,7 +140,7 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_WRITE);
   if (mgr == nullptr) {
     // can't open service manager
-    throw XArchDaemonUninstallFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonUninstallFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service.  oddly, you must open a service to delete it.
@@ -149,9 +149,9 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
     DWORD err = GetLastError();
     CloseServiceHandle(mgr);
     if (err != ERROR_SERVICE_DOES_NOT_EXIST) {
-      throw XArchDaemonUninstallFailed(windowsErrorToString(err));
+      throw ArchDaemonUninstallFailedException(windowsErrorToString(err));
     }
-    throw XArchDaemonUninstallNotInstalled(windowsErrorToString(err));
+    throw ArchDaemonUninstallNotInstalledException(windowsErrorToString(err));
   }
 
   // stop the service.  we don't care if we fail.
@@ -184,9 +184,9 @@ void ArchDaemonWindows::uninstallDaemon(const char *name)
       return;
     }
     if (err != ERROR_SERVICE_MARKED_FOR_DELETE) {
-      throw XArchDaemonUninstallFailed(windowsErrorToString(err));
+      throw ArchDaemonUninstallFailedException(windowsErrorToString(err));
     }
-    throw XArchDaemonUninstallNotInstalled(windowsErrorToString(err));
+    throw ArchDaemonUninstallNotInstalledException(windowsErrorToString(err));
   }
 }
 
@@ -211,7 +211,7 @@ int ArchDaemonWindows::daemonize(const char *name, DaemonFunc const &func)
   if (StartServiceCtrlDispatcher(entry) == 0) {
     // StartServiceCtrlDispatcher failed
     s_daemon = nullptr;
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   s_daemon = nullptr;
@@ -472,7 +472,7 @@ void ArchDaemonWindows::serviceMain(DWORD argc, LPTSTR *argvIn)
   try {
     // invoke daemon function
     m_daemonResult = m_daemonFunc(static_cast<int>(argc), argv);
-  } catch (XArchDaemonRunFailed &e) {
+  } catch (ArchDaemonRunException &e) {
     setStatusError(e.m_result);
     m_daemonResult = -1;
   } catch (...) {
@@ -560,7 +560,7 @@ void ArchDaemonWindows::start(const char *name)
   // open service manager
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_READ);
   if (mgr == nullptr) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service
@@ -568,12 +568,12 @@ void ArchDaemonWindows::start(const char *name)
 
   if (service == nullptr) {
     CloseServiceHandle(mgr);
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // start the service
   if (!StartService(service, 0, nullptr)) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 }
 
@@ -582,7 +582,7 @@ void ArchDaemonWindows::stop(const char *name)
   // open service manager
   SC_HANDLE mgr = OpenSCManager(nullptr, nullptr, GENERIC_READ);
   if (mgr == nullptr) {
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // open the service
@@ -590,7 +590,7 @@ void ArchDaemonWindows::stop(const char *name)
 
   if (service == nullptr) {
     CloseServiceHandle(mgr);
-    throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+    throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
   }
 
   // ask the service to stop, asynchronously
@@ -598,7 +598,7 @@ void ArchDaemonWindows::stop(const char *name)
   if (!ControlService(service, SERVICE_CONTROL_STOP, &ss)) {
     DWORD dwErrCode = GetLastError();
     if (dwErrCode != ERROR_SERVICE_NOT_ACTIVE) {
-      throw XArchDaemonFailed(windowsErrorToString(GetLastError()));
+      throw ArchDaemonFailedException(windowsErrorToString(GetLastError()));
     }
   }
 }