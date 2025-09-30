deskflow/deskflow

refactor: move tls-cert to coreArgs

src/lib/deskflow/CoreArgs.h
@@ -40,6 +40,9 @@ struct CoreArgs
 
   inline static const auto insecureOption = QCommandLineOption("insecure", "Disable TLS encryption");
 
+  inline static const auto tlsCertOption =
+      QCommandLineOption("tls-cert", "Use file in place of default TLS certificate path", "file");
+
   inline static const auto options = {helpOption, versionOption,  configOption,  interfaceOption, portOption,
-                                      nameOption, logLevelOption, logFileOption, insecureOption};
+                                      nameOption, logLevelOption, logFileOption, insecureOption,  tlsCertOption};
 };

lockekk/dshare-hid

src/lib/deskflow/CoreArgs.h
@@ -41,6 +41,9 @@ struct CoreArgs
   inline static const auto secureOption =
       QCommandLineOption("secure", "Enable TLS encryption (default: true)", "value");
 
+  inline static const auto tlsCertOption =
+      QCommandLineOption("tls-cert", "Use file in place of default TLS certificate path", "file");
+
   inline static const auto options = {helpOption, versionOption,  configOption,  interfaceOption, portOption,
-                                      nameOption, logLevelOption, logFileOption, secureOption};
+                                      nameOption, logLevelOption, logFileOption, secureOption,  tlsCertOption};
 };